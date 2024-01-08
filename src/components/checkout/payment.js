0190fef72e0d3d860d411395536663632757a833
Yewandee <88537289+Yewandee@users.noreply.github.com>
2024-01-08 14:16:15 +0100

    Payment

--- src/components/checkout/payment.js
+++ src/components/checkout/payment.js
@@ -4,6 +4,7 @@ import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
+import './payment.css'
 
 const Payment = () => {
   return (
@@ -11,62 +12,114 @@ const Payment = () => {
       <Typography variant="h6" gutterBottom>
         Select a Payment method
       </Typography>
-      <Grid container spacing={3}>
-       
-       
-       
-       
-       
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cardName"
-            label="Name on card"
-            fullWidth
-            autoComplete="cc-name"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cardNumber"
-            label="Card number"
-            fullWidth
-            autoComplete="cc-number"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="expDate"
-            label="Expiry date"
-            fullWidth
-            autoComplete="cc-exp"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cvv"
-            label="CVV"
-            helperText="Last three digits on signature strip"
-            fullWidth
-            autoComplete="cc-csc"
-            variant="standard"
-          />
-        </Grid>
+      <Grid container spacing={3} className='mt-5'>
         <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveCard" value="yes" />}
-            label="Remember credit card details for next time"
-          />
+          <div class="accordion accordion-flush">
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="flush-headingOne">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                  Bank Transfer
+                </button>
+              </h2>
+              <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body">
+                  Bank: Union Bank
+                  <br />
+                  Account Number: 0111209968
+                  <br />
+                  Account Name: BIOBAKU YEWANDE BASIRAT
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="flush-headingTwo">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                  Bank Account
+                </button>
+              </h2>
+              <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body">
+                  
+                  </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="flush-headingThree">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                  Card
+                </button>
+              </h2>
+              <Grid item xs={12} >
+                <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                  <div class="accordion-body">
+                    <Grid item xs={12} md={6}>
+                      <TextField
+                        required
+                        id="cardName"
+                        label="Name on card"
+                        fullWidth
+                        autoComplete="cc-name"
+                        variant="standard"
+                      />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                      <TextField
+                        required
+                        id="cardNumber"
+                        label="Card number"
+                        fullWidth
+                        autoComplete="cc-number"
+                        variant="standard"
+                      />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                      <TextField
+                        required
+                        id="expDate"
+                        label="Expiry date"
+                        fullWidth
+                        autoComplete="cc-exp"
+                        variant="standard"
+                      />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                      <TextField
+                        required
+                        id="cvv"
+                        label="CVV"
+                        helperText="Last three digits on signature strip"
+                        fullWidth
+                        autoComplete="cc-csc"
+                        variant="standard"
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <FormControlLabel
+                        control={<Checkbox color="secondary" name="saveCard" value="yes" />}
+                        label="Remember credit card details for next time"
+                      />
+                    </Grid>
+                  </div>
+                </div>
+              </Grid>
+            </div>
+          </div>
         </Grid>
+
+
+
+        {/* <Grid item xs={12} className='option'>
+          Bank Transfer
+        </Grid>
+        <Grid item xs={12} className='option'>
+          Card
+        </Grid>
+        <Grid item xs={12} className='option'>
+          Bank Account
+        </Grid> */}
       </Grid>
     </React.Fragment>
-    
+
   )
 }
 
