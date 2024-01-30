1be603e5c3b74c4179cd66cf813a6b81db0eeb2b
Yewandee <88537289+Yewandee@users.noreply.github.com>
2024-01-30 14:20:11 +0100

    Feature

--- src/components/features/features.js
+++ src/components/features/features.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
+import img1 from '../../assets/images/img14.png'
+
 const Features = () => {
   return (
-    <div>Features</div>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-12 col-md-4 card'>
+            <img src={img1} />        
+        </div>
+        <div className='col-12 col-md-4 card'>
+        <img src={img1} />  
+        </div>
+        <div className='col-12 col-md-4 card'>
+        <img src={img1} />  
+        </div>
+       
+      </div>
+    </ div>
   )
 }
 
